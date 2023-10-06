24c2eefddb252f8654f07b11e8dc842da5b19f14
za1914 <147187825+za1914@users.noreply.github.com>
2023-10-06 12:55:44 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1006365672378667008"; // your league ID
+export const leagueID = "919626385692381184"; // your league ID
 export const leagueName = "Wisco Dynasty"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
